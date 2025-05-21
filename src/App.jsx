4a64b5586bddd2211caabb67ae0da7e57917d118
kikoolz <kenlubs45@gmail.com>
2025-05-22 03:22:48 +0400

    refactor: call setCurrentWord before setGuessedLetters in handleNewGame to align update sequence

--- src/App.jsx
+++ src/App.jsx
@@ -30,8 +30,8 @@ export default function AssemblyEndgame() {
   }
 
   function handleNewGame() {
-    setGuessedLetters([]);
     setCurrentWord(getRandomWord());
+    setGuessedLetters([]);
   }
 
   const languageElements = languages.map((lang, index) => {
